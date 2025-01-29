fdc921a4d5db5a233fe14937b6555967f09c48ed
gautamSH20 <gs841400@gmail.com>
2025-01-29 17:04:37 +0530

    new backendurl updated

--- src/utils/url.tsx
+++ src/utils/url.tsx
@@ -1,1 +1,5 @@
-export const BACKEND_URL = "http://localhost:3001";
+const IS_PROD: boolean = true;
+
+export const BACKEND_URL = IS_PROD
+  ? "https://in-yt-mp4-backend.onrender.com"
+  : "http://localhost:3001";
